d1eaa66d4fa47e13903d3567adc6a7d688aeea4f
ieneiraul <73996281+ieneiraul@users.noreply.github.com>
2023-09-25 18:26:25 +0300

    Add files via upload

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,59 @@
+import {Component} from "@angular/core";
+import {NgForm} from "@angular/forms";
+import {AuthResponseData, AuthService} from "./auth.service";
+import {Observable} from "rxjs";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html'
+})
+export class AuthComponent {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(authForm: NgForm) {
+    if(!authForm.valid) {
+      return;
+    }
+
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+    if(this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
+    }
+
+    authObs.subscribe(
+      responseData => {
+        console.log(responseData);
+        this.error = null;
+        this.isLoading = false;
+        this.router.navigate(['/recipes']);
+      }, errorRes => {
+        console.log(errorRes);
+        this.error = errorRes;
+        this.isLoading = false;
+      }
+    );
+
+    authForm.reset();
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+}
---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-loading-spinner',
+  template: '<div class="loadingio-spinner-blocks-j0oy6qvheo"><div class="ldio-2y5g8li88jo">\n' +
+    '<div style=\'left:38px;top:38px;animation-delay:0s\'></div><div style=\'left:80px;top:38px;animation-delay:0.125s\'></div><div style=\'left:122px;top:38px;animation-delay:0.25s\'></div><div style=\'left:38px;top:80px;animation-delay:0.875s\'></div><div style=\'left:122px;top:80px;animation-delay:0.375s\'></div><div style=\'left:38px;top:122px;animation-delay:0.75s\'></div><div style=\'left:80px;top:122px;animation-delay:0.625s\'></div><div style=\'left:122px;top:122px;animation-delay:0.5s\'></div>\n' +
+    '</div></div>',
+  styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent {
+
+}
